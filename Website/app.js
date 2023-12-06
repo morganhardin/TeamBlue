4de24912cb4f6df7af631be367fa6441a58e33bf
TylerCambronGH <tcambron@bellarmine.edu>
2023-12-06 03:23:09 -0500

    Connecting calendar to table view

--- Website/app.js
+++ Website/app.js
@@ -1,18 +1,6 @@
-//const http = require('http')
-
 const express = require('express')
 const hostname = '0.0.0.0'
 const port = 3000
-
-/*const server = http.createServer((req, res) => {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/plain')
-    res.end('SmartReserve\n')
-})
-
-server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
-})*/
 
 const app = module.exports = express()
 
--- Website/assets/js/calendar.js
+++ Website/assets/js/calendar.js
@@ -1,12 +1,32 @@
 const MONTHS = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
+const username = 'admin'
+const password = 'admin'
+
+var loggedIn = false
+
 var month = 1
 var year = 2024
 
+var tables = {}
+
 $(function(){
+    $("#timeslot-view-header").hide()
+
     var currentDay = new Date()
     month = currentDay.getMonth()+1
     year = currentDay.getFullYear()
+
+    if (tables[year] == null) {
+        tables[year] = {}
+    }
+    if (tables[year][month] == null) {
+        tables[year][month] = {}
+    }
+
+    $("#log-in-btn").click(function() {
+        restaurantLogIn()
+    })
 
     $("#calendar-month-current").click(function(){
         console.log("Number of days in month " + MONTHS[month-1] + " of the year " + year + " is " + getDaysInMonth(year, month));
@@ -27,9 +47,12 @@ $(function(){
         }
         buildCalendar(year, month)
     })
-    // CREATE CALENDAR
-    $('#calendar-grid').on('click', '.calendar-day', function() {
-        
+    $('#calendar-grid').on('click', '.calendar-day-timeslot', function() {
+        var day = $(this).data('day')
+        var time = $(this).data('time') + ":00"
+        $("#timeslot-view-header").show()
+        $("#timeslot-view-info").text(time + " | " + day + " | " + MONTHS[month-1] + " | " + year)
+        showTimeSlot(parseInt(day), parseInt(time))
     });
     buildCalendar(year, month)
 });
@@ -49,19 +72,28 @@ function buildCalendar(year, month) {
 
     for (let i = 1; i <= days; i++) {
         var timeslotClass = 'open'
-        var seatsOpen = 10
-        console.log(currentDay.getDate())
+        if (tables[year][month][i] == null) {
+            tables[year][month][i] = {}
+        }
+        //console.log(currentDay.getDate())
         if (currentDay.getFullYear() > year || 
         (currentDay.getFullYear() == year && currentDay.getMonth()+1 > month) || 
         (currentDay.getFullYear() == year && currentDay.getMonth()+1 == month && currentDay.getDate() > i)) {
             timeslotClass = 'closed'
-            seatsOpen = 0
         }
 
         var calInner = `<div>`+ i +`</div>`
         for (let j = 1; j <= 10; j+=2) {
+            if (tables[year][month][i][j] == null) {
+                tables[year][month][i][j] = 10
+            }
+            if (timeslotClass === 'closed') {
+                tables[year][month][i][j] = 0
+            }
             calInner += `
-                <div class="calendar-day-timeslot timeslot-`+timeslotClass+`">`+ j + ':00' + ' (' + seatsOpen + ')' + `</div>
+                <div class="calendar-day-timeslot timeslot-`+timeslotClass+`" 
+                    data-day=`+i+` data-time=`+j+`
+                >`+ j + ':00' + ' (' + tables[year][month][i][j] + ')' + `</div>
             `
         }
         calendarHtml += `<div id="calendar-day-`+i+`" class="calendar-day col-sm-2 border">`+calInner+`</div>`
@@ -69,4 +101,29 @@ function buildCalendar(year, month) {
     calendarHtml += `</div>`
 
     $('#calendar-grid').html(calendarHtml)
+}
+
+function showTimeSlot(day, timeslot) {
+    if (tables[year][month][day][timeslot] === 0) {
+        $("#timeslot-view-tablesleft").text("No tables left!")
+        $("#timeslot-register").text("")
+    } else {
+        $("#timeslot-view-tablesleft").text(tables[year][month][day][timeslot] + " tables available.")
+        $("#timeslot-register").attr("href", "tabletracker.html?year="+year+"?&month="+month+"&day="+day)
+        $("#timeslot-register").text("Sign up for a table.")
+    }
+}
+
+function restaurantLogIn() {
+    var uname = $("#log-in-uname").val()
+    $("#log-in-uname").val("")
+    var pwd = $("#log-in-pwd").val()
+    $("#log-in-pwd").val("")
+    if (uname === username && pwd === password) {
+        loggedIn = true
+        $("#log-in-header").hide()
+        $("#log-in-body").hide()
+    } else {
+        $("#log-in-header").css("color", "red")
+    }
 }
